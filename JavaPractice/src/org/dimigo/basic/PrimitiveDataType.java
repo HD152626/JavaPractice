b21f0bc4ee81c2421c3adf4b65081fd7d2f0cc76
Weed <Weed@Weed>
2017-03-13 12:28:13 +0900

    실습과제1 제출

---
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_PrimitveDataType
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 13.
+ * </pre>
+ * 
+ * @author				:  Weed
+ * @version				:  1.0
+ */
+public class PrimitiveDataType {
+	public static void main(String[] args) {
+		String name = "이지은";
+		boolean isMale = false;
+		int age = 25;
+		double height = 161.8;
+		float weight = (float)44.3;
+		char bloodType = 'A';
+		
+		System.out.println("    <<아이유 프로필>>");
+		System.out.println(" 이름     : " + name);
+		if(isMale) System.out.println(" 성별     : 남자");
+		else System.out.println(" 성별     : 여자");
+		System.out.println(" 나이     : " + age);
+		System.out.println(" 신장     : " + height + "cm");
+		System.out.println(" 체중     : " + weight + "kg");
+		System.out.println(" 혈액형 : " + bloodType + "형");
+	}
+}
