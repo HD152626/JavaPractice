e9958dfb9a48c377e0c4eec954794b1dfd7a0654
Weed <Weed@Weed>
2017-03-13 12:42:22 +0900

    실습과제2 제출

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_TypeCasting
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 13.
+ * </pre>
+ * 
+ * @author				:  Weed
+ * @version				:  1.0
+ */
+public class Operator {
+	public static void main(String[] args) {
+		 int monthPay = 1700000, clerk = 3, shop = 1500;
+		 System.out.println("  << 디미베네 연간 인건비 >>");
+		 System.out.println(" 월 평균 급여 : " + String.format("%,d", monthPay) + "원");
+		 System.out.println(" 점포 내 직원 수 : " + clerk + "명");
+		 System.out.println(" 점포 수 : " + String.format("%,d", shop) + "개\n");
+		 long yearPay = monthPay * 12L * clerk * shop;
+		 System.out.println(" 연간 인건비 : " + String.format("%,d", yearPay) + "원");
+	}
+}
